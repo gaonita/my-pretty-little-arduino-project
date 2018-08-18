58763d3f4f3e84114394d7c9199cf962da11c913
Gaon <gaon.yg@gmail.com>
2018-08-18 20:31:24 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <Adafruit_CPlay_NeoPixel.h>
+
+Adafruit_CPlay_NeoPixel pixels = Adafruit_CPlay_NeoPixel(1, 2, NEO_GRB);
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
